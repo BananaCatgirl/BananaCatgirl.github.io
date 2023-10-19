8c34054547b75d7ec6823d9154ca87541b0fa1d6
Jenny <jenny.bananatoast@gmail.com>
2023-10-19 14:15:55 +0200

    lol

--- Scripts.js
+++ Scripts.js
@@ -1,9 +1,13 @@
 window.onscroll = function () { TopNavScroll() };
 
 function TopNavScroll() {
-	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+	if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 80) {
 		document.getElementById("topnav").style.padding = "10px 10px";
 	} else {
 		document.getElementById("topnav").style.padding = "30px 10px";
 	}
+}
+
+function DesignToggle() {
+
 }
